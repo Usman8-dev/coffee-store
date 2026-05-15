4be29f6e74b50ca9f26fce05779c6724417fc416
Usman <abc.usman.100@gmail.com>
2026-05-15 16:46:56 +0500

    image Bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,13 @@ export default function Home() {
       name: "Dark Coffee",
       imgUrl: "/static/hero-image.png",
     },
-    
   ];
 
   return (
     <div className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 w-full min-h-screen">
-      
       <main className="max-w-7xl mx-auto px-6 md:px-16 py-12">
-        
         {/* Hero Section */}
         <section className="flex flex-col-reverse lg:flex-row items-center justify-between gap-16 pt-1">
-          
           {/* Left Content */}
           <div className="flex-1">
             <Banner />
@@ -43,55 +39,45 @@ export default function Home() {
               "
             />
           </div>
-
         </section>
 
         {/* Coffee Store Cards */}
         <section className="py-16">
-          
-          <h2 className="text-4xl font-bold text-white mb-10">
-            Coffee Stores
-          </h2>
+          <h2 className="text-4xl font-bold text-white mb-10">Islamabad Stores</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-            
-       
-              <Card
-              name="ABC"
+            <Card
+              name="Flat White"
               imgUrl="/static/hero-image.png"
               href="/coffee-store/1"
-              />
-              <Card
-              name="Dark Coffee"
-              imgUrl="/static/hero-image.png"
+            />
+            <Card
+              name="Espresso"
+              imgUrl="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D"
               href="/coffee-store/1"
-              />
-              <Card
-              name="ABC"
-              imgUrl="/static/hero-image.png"
+            />
+            <Card
+              name="Americano "
+              imgUrl="https://images.unsplash.com/photo-1512568400610-62da28bc8a13?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D"
               href="/coffee-store/1"
-              />
-              <Card
-              name="Dark Coffee"
-              imgUrl="/static/hero-image.png"
+            />
+            <Card
+              name="Cappuccino "
+              imgUrl="https://images.unsplash.com/photo-1461023058943-07fcbe16d735?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D"
               href="/coffee-store/1"
-              />
-              <Card
-              name="ABC"
-              imgUrl="/static/hero-image.png"
+            />
+            <Card
+              name="Latte "
+              imgUrl="https://images.unsplash.com/photo-1562447457-579fc34967fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D"
               href="/coffee-store/1"
-              />
-              <Card
-              name="Dark Coffee"
-              imgUrl="/static/hero-image.png"
+            />
+            <Card
+              name="Macchiato "
+              imgUrl="https://images.unsplash.com/photo-1498804103079-a6351b050096?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D"
               href="/coffee-store/1"
-              />
-          
-
+            />
           </div>
-
         </section>
-
       </main>
     </div>
   );
